116802fb959ebb19f3dcc235f262520cf5e61d7f
annajg <wtzhell@gmail.com>
2019-03-27 16:59:58 +0300

    azaza

--- js/script.js
+++ js/script.js
@@ -1,6 +1,20 @@
-let money = prompt("Ваш бюджет?");
-let name = prompt("Название вашего магазина?");
-let time = 19;
+let money,
+name,
+time
+
+function start(){
+	money = prompt("Ваш бюджет? 22222");
+
+	while(isNaN(money) || money == "" || money == null) {
+		money = prompt("Ваш бюджет?");
+
+	}
+
+	name = prompt("Название вашего магазина?").toUpperCase();
+	time = 21;
+
+}
+start();
 
 let mainList = {
 	budget: money,
@@ -10,30 +24,30 @@ let mainList = {
 	open: false
 }
 
+function chooseGoods() {
+	for (let i = 0; i < 5; i++) {
+		let a = prompt("Какой тип товаров будем продавать?");
 
-for (let i = 0; i < 5; i++) {
-
-	let a = prompt("Какой тип товаров будем продавать?");
-	mainList.shopGoods[i] = a;
-	if ((typeof(a)) ==="string" && ((typeof(a))) === null && a != '' && a.length < 50 ) {
-		console.log('Good!');
-		mainList.shopGoods[i] = a;
-	} else {
-
+		if ((typeof(a)) ==="string" && ((typeof(a))) != null && a != '' && a.length < 50 ) {
+			console.log('Good!');
+			mainList.shopGoods[i] = a;
+		} else {
+			i = i - 1;		
+		}
 	}
-
 }
+chooseGoods();
 
-if (time < 0) {
-	console.log('R U fuckin kiddin me?');
-} else if (time > 8 && time < 20) {
-	console.log('Work, bitch!')
-    } else if (time < 24) {
-    	console.log('Too late');
-        } else {
-            console.log('Not on this planet');
-        };
+	if (time < 0) {
+		console.log('R U fuckin kiddin me?');
+	} else if (time > 8 && time < 20) {
+		console.log('Work, bitch!')
+	} else if (time < 24) {
+		console.log('Too late');
+	} else {
+		console.log('Not on this planet');
+	};
 
-alert(mainList.budget / 30);
+	alert(mainList.budget / 30);
 
-console.log(mainList);
+	console.log(mainList);
